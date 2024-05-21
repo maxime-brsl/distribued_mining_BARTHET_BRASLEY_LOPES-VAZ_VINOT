fa4ad288240b2dbf2c14d5f797d7b26de942e838
lopesvaz3u <82098184+lopesvaz3u@users.noreply.github.com>
2024-05-21 22:32:05 +0200

    ajout de tous les messages possibles

--- src/Messages.java
+++ src/Messages.java
@@ -6,4 +6,12 @@ public class Messages {
     public static final String YOU_DONT_FOOL_ME = "YOU_DONT_FOOL_ME";
     public static final String OK = "OK";
     public static final String READY = "READY";
+    public static final String SOLVED = "SOLVED";
+    public static final String PROGRESS = "PROGRESS";
+    public static final String CANCELLED = "CANCELLED";
+    public static final String NONCE = "NONCE";
+    public static final String PAYLOAD = "PAYLOAD";
+    public static final String SOLVE = "SOLVE";
+    public static final String FOUND = "FOUND";
+    public static final String NOPE = "NOPE";
 }
